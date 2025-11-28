85656830573c74e779a95bb970521afac7dd127a
evanotebook <eva.masakova@post.cz>
2025-11-28 12:41:42 +0100

    detail page

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -4,13 +4,11 @@ import GoBackButton from "../components/buttons/GoBackButton.jsx";
 import DetailPageHeader from "../components/detailPage/detailPageHeader.jsx";
 import AboutPanel from "../components/detailPage/AboutPanel.jsx";
 import QandA from "../components/detailPage/QandA.jsx";
-import ProgressBar from "../components/detailPage/ProgressBar.jsx";
 export default function DetailPage() {
   return (
     <div className="min-h-screen py-10 space-y-8">
       <GoBackButton />
       <DetailPageHeader />
-      <ProgressBar />
       <AboutPanel />
       <QandA />
     </div>
