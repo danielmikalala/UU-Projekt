ec94fce439fb62f1a31e0279679a8bc1964b645e
Simon. <73310668+simonp-stokuc@users.noreply.github.com>
2025-12-02 15:57:14 +0100

    feat: detail api integration

---
+++ src/components/detailPage/DonatePanel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProgressBar from "../ProgressBar.jsx";
+
+export default function DonatePanel() {
+    return (
+        <div className="w-full max-w-6xl mx-auto border rounded-xl p-6 bg-white shadow-sm">
+            <ProgressBar/>
+
+        </div>
+    );
+}
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,17 +1,35 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, {useEffect} from "react";
+import {useParams} from "react-router-dom";
 import GoBackButton from "../components/buttons/GoBackButton.jsx";
 import DetailPageHeader from "../components/detailPage/detailPageHeader.jsx";
 import AboutPanel from "../components/detailPage/AboutPanel.jsx";
 import QandA from "../components/detailPage/QandA.jsx";
+import {useAuth} from "../context/AuthContext.jsx";
+import ProgressBar from "../components/ProgressBar.jsx";
+
 export default function DetailPage() {
-  return (
-    <div className="min-h-screen py-10 space-y-8">
-      <GoBackButton />
-      <DetailPageHeader />
-      <AboutPanel /> 
-      <QandA />
-    </div>
-  );
+    const {token} = useAuth();
+    const {id} = useParams();
+    const fetchDetailData = async () => {
+        const res = await fetch(`/project/${id}`, {
+            method: "GET",
+            headers: {"Content-Type": "application/json", 'Authorization': `Bearer ${token}`},
+        });
+
+        return res.body;
+    }
+    const [detailData, setDetailData] = React.useState(fetchDetailData());
+    useEffect(() => {
+        setDetailData(fetchDetailData());
+    }, [id]);
+    return (
+        <div className="min-h-screen py-10 space-y-8">
+            <GoBackButton/>
+            <ProgressBar goal={detailData.goalAmount} status={0}></ProgressBar>
+            <DetailPageHeader/>
+            <AboutPanel/>
+            <QandA/>
+        </div>
+    );
 
 }
